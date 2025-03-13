de5356e3694eae79f66022f095d81e9a83821327
Victor_Vaz <63057845+VictorVaz-Dev@users.noreply.github.com>
2025-03-13 15:53:41 -0300

    subindo o npm build

--- npm/src/routes/Rotas.tsx
+++ npm/src/routes/Rotas.tsx
@@ -1,7 +1,7 @@
 import {Routes, Route} from 'react-router-dom';
 import Login from '../pages/login/Login';
 import Cadastro from '../pages/cadastro/Cadastro';
-import Home from '../pages/home/Home';
+
 
 
 export default function Rotas(){
