fd35d1f1b3c34902a58b4382d5741d014413a6cb
Victor_Vaz <63057845+VictorVaz-Dev@users.noreply.github.com>
2025-03-14 01:20:54 -0300

    subindo segurança para as chaves
    .

--- npm/src/services/firebase.ts
+++ npm/src/services/firebase.ts
@@ -9,13 +9,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCVZYo7jq0IVPVO8p8lCMgIpO9zSymshMc",
-  authDomain: "xweb-2013d.firebaseapp.com",
-  projectId: "xweb-2013d",
-  storageBucket: "xweb-2013d.firebasestorage.app",
-  messagingSenderId: "64656416553",
-  appId: "1:64656416553:web:df143e9e93952699e9e138",
-  measurementId: "G-V9VGMXV9WN"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
